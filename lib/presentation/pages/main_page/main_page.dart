6089f3bb438f3aa4031df54d23aa703af034d821
albae69 <ahmadalbaihaqi69@gmail.com>
2024-07-09 14:20:19 +0700

    added: vide0 capture

--- lib/presentation/pages/main_page/main_page.dart
+++ lib/presentation/pages/main_page/main_page.dart
@@ -1,6 +1,10 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
+import 'package:groceries_app/config/config.dart';
+import 'package:groceries_app/presentation/misc/methods.dart';
+import 'package:groceries_app/presentation/providers/router/router_provider.dart';
 import 'package:groceries_app/presentation/providers/user_data/user_data_provider.dart';
+import 'package:shared_preferences/shared_preferences.dart';
 
 class MainPage extends ConsumerWidget {
   const MainPage({super.key});
@@ -12,11 +16,49 @@ class MainPage extends ConsumerWidget {
       print('userDataProvider next: $next');
     });
 
+    void onLogout() async {
+      final SharedPreferences prefs = await SharedPreferences.getInstance();
+      prefs.setBool(Config.isLoggedIn, false);
+      prefs.setString(Config.token, '');
+      ref.read(routerProvider).replaceNamed('splash');
+    }
+
     return Scaffold(
-      body: Column(
-        mainAxisAlignment: MainAxisAlignment.center,
-        crossAxisAlignment: CrossAxisAlignment.center,
-        children: [],
+      body: Padding(
+        padding: const EdgeInsets.symmetric(horizontal: 16),
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          crossAxisAlignment: CrossAxisAlignment.center,
+          children: [
+            Text(
+              'Hello, ${ref.read(userDataProvider).value?.name}',
+              style: TextStyle(
+                fontSize: 24,
+                fontWeight: FontWeight.w600,
+              ),
+            ),
+            verticalSpace(30),
+            Container(
+              height: 57,
+              width: double.infinity,
+              child: ElevatedButton(
+                onPressed: onLogout,
+                style: ElevatedButton.styleFrom(
+                    shape: RoundedRectangleBorder(
+                      borderRadius: BorderRadius.circular(20),
+                    ),
+                    backgroundColor: ThemeConfig.primaryColor),
+                child: const Text(
+                  'Log Out',
+                  style: TextStyle(
+                    fontSize: 18,
+                    color: Colors.white,
+                  ),
+                ),
+              ),
+            )
+          ],
+        ),
       ),
     );
   }
